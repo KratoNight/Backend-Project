e889fe7a106644d12a65d3a54fffb619d4d6847a
KratoNight <josh-pearson@live.co.uk>
2024-04-18 14:08:20 +0100

    task 7 complete, testing, express.json etc)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -150,3 +150,71 @@ describe('/api/articles/:article_id/comments', () => {
         })
     })
 })
+describe('/api/articles/:article_id/comments', () => {
+    test('POST 201: Responds with a comment object that has been inserted into the comments table and contains the relevent information', () => {
+        const fakeCommentForTesting = { username: 'lurker', body: 'Just a random comment for testing purposes!'}
+        return request(app)
+        .post('/api/articles/1/comments')
+        .send(fakeCommentForTesting)
+        .expect(201)
+        .then(({ body : { comment } }) => {
+            const { comment_id, author, body, article_id, votes, created_at }  = comment
+            expect(comment_id).toBe(19)
+            expect(article_id).toBe(1)
+            expect(author).toBe('lurker')
+            expect(body).toBe('Just a random comment for testing purposes!')
+            expect(votes).toBe(0)
+            expect(typeof created_at).toBe('string')        
+        })
+    })
+    test('POST 404: Responds with a 404 Not Found! error when the request body is sent a unknown/not known username which doesn\'t exist in the db', () => {
+        const fakeCommentForTesting = {username: 'meowmeowingway', body: 'Just a random comment for testing purposes!'}
+        return request(app)
+        .post('/api/articles/1/comments')
+        .send(fakeCommentForTesting)
+        .expect(404)
+        .then(({ body }) => {
+        expect(body.msg).toBe('Not found!')
+        })
+    })
+    test('POST 404: Responds with a not found error if article_id is valid but does not exist in db', () => {
+        const fakeCommentForTesting = { username: 'lurker', body: 'Just a random comment for testing purposes!'}
+        return request(app)
+        .post('/api/articles/222/comments')
+        .send(fakeCommentForTesting)
+        .expect(404)
+        .then(({ body }) => {
+        expect(body.msg).toBe('Not found!')
+        })
+    })
+    test('POST 400: Responds with a bad request if the username is valid but the object key that is being passed in does not match!', () => {
+        const fakeCommentForTesting = { username: 'lurker', someRandomBody : 'Just a random comment for testing purposes!'}
+        return request(app)
+        .post('/api/articles/1/comments')
+        .send(fakeCommentForTesting)
+        .expect(400)
+        .then(({ body }) => {
+        expect(body.msg).toBe('Bad request!')
+        })
+    })
+    test('Post 400: Responds with bad request if just passed the body', () => {
+        const fakeCommentForTesting = { body: 'Just a random comment for testing purposes!'}
+        return request(app)
+          .post("/api/articles/1/comments")
+          .send(fakeCommentForTesting)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Bad request!');
+          });
+      });
+      test('Post 400: Responds with bad request if just passed the username with no comment assigned to it!', () => {
+        const fakeCommentForTesting = { username: 'lurker'}
+        return request(app)
+          .post("/api/articles/1/comments")
+          .send(fakeCommentForTesting)
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe('Bad request!');
+          });
+      });
+})
